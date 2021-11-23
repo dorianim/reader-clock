945fcd4f353d88a5f694d5e0681e44579d9bf994
16flotho <thomas.florian@tfobz.net>
2021-11-24 00:23:25 +0100

    feat: add quotes

---
+++ Quotes/generateQuotes.py
@@ -0,0 +1,23 @@
+import re
+
+quoteArray = "Quote quotes[] = {\n"
+
+with open('quotes.csv', encoding="utf-8") as f: 
+    for line in f:
+        l = line.rstrip()
+        l = re.sub(r'(\"|\'|‘|“)+', "\\\"", l)
+        quote = l.split("|")
+        start = quote[2].find(quote[1])
+        end = start + len(quote[1])
+        string = quote[2][:start] + "|" + quote[2][start:end] + "|"+ quote[2][end:]
+        time = quote[0].split(":")
+        quoteArray += "Quote{\"" + string + "\", \"" + quote[4] + "\", \"" + quote[3] + "\", " + time[0] + ", " + time[1] + "},\n"
+
+quoteArray += "};"
+
+f = open("quoteObject.txt", "w", encoding="utf-8")
+f.write(quoteArray)
+f.close()
+        
+
+
