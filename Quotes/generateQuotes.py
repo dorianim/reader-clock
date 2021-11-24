b9b20906c2c9f0dbc52a3f45dea69b550f4b67e1
16flotho <thomas.florian@tfobz.net>
2021-11-24 22:33:54 +0100

    feat: minor changes in the script

--- Quotes/generateQuotes.py
+++ Quotes/generateQuotes.py
@@ -1,17 +1,18 @@
 import re
 
-quoteArray = "Quote quotes[] = {\n"
+quoteArray = "QuoteData QuoteDatas[] = {\n"
 
 with open('quotes.csv', encoding="utf-8") as f: 
     for line in f:
         l = line.rstrip()
         l = re.sub(r'(\"|‘|“|”)+', "\\\"", l)
+        l = re.sub(r'(‘|’)+', "'", l)
         quote = l.split("|")
-        start = quote[2].find(quote[1])
+        start = quote[2].lower().find(quote[1].lower())
         end = start + len(quote[1])
         string = quote[2][:start] + "|" + quote[2][start:end] + "|"+ quote[2][end:]
         time = quote[0].split(":")
-        quoteArray += "Quote{\"" + string + "\", \"" + quote[4] + "\", \"" + quote[3] + "\", " + str(int(time[0])) + ", " + str(int(time[1])) + "},\n"
+        quoteArray += "QuoteData{\"" + string + "\", \"" + quote[4] + "\", \"" + quote[3] + "\", " + str(int(time[0])) + ", " + str(int(time[1])) + ", false" + "},\n"
 
 quoteArray += "};"
 
